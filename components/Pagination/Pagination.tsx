a101f087f4b67e72ed4abdf95e5cffcef9232d3b
vjosik <108922728+vjosik@users.noreply.github.com>
2026-03-22 19:40:59 +0200

    hm6

---
+++ components/Pagination/Pagination.tsx
@@ -0,0 +1,34 @@
+import ReactPaginate from "react-paginate";
+import css from "./Pagination.module.css";
+
+const Paginate = (ReactPaginate as any).default || ReactPaginate;
+
+interface PaginationProps {
+  page: number;
+  totalPages: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+interface PageChangeEvent {
+  selected: number;
+}
+
+export default function Pagination({
+  page,
+  totalPages,
+  setPage,
+}: PaginationProps) {
+  return (
+    <Paginate
+      pageCount={totalPages}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected }: PageChangeEvent) => setPage(selected + 1)}
+      forcePage={page - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+    />
+  );
+}
